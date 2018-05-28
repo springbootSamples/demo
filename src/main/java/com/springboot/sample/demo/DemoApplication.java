34630373dfddd78d3311a4e1a82d4d5a773c280d
shinshin <iamshb@linecorp.com>
2018-05-29 02:10:06 +0900

    change .gitignore

--- src/main/java/com/springboot/sample/demo/DemoApplication.java
+++ src/main/java/com/springboot/sample/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
+		System.out.println("Hello SpringBoot with MSCode.");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
